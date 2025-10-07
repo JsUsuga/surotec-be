76a491fa5cd8d4f73d2a785b11abd2af31e8b81d
Samuel Ospina Jaramillo <samuel.ospina@globant.com>
2025-10-06 20:40:27 -0500

    Se arreglo la entidad employee

--- surotec-academy/src/main/java/com/website/surotec_academy/repository/EmployeeRepository.java
+++ surotec-academy/src/main/java/com/website/surotec_academy/repository/EmployeeRepository.java
@@ -1,6 +1,5 @@
 package com.website.surotec_academy.repository;
 
-import com.website.surotec_academy.enums.EmStatus;
 import com.website.surotec_academy.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,7 +9,6 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<StudentEntity, Long> {
 
-    List<EmployeeEntity> findByStatus(EmployeeStatus status);
 
 
 }
